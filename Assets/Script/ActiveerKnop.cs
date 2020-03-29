4813babd18cd853eb001a3d4025737692e8d6b0c
Anna-TheConceptArtist <annsie@live.nl>
2020-03-29 09:53:34 +0200

    Gehele game

---
+++ Assets/Script/ActiveerKnop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ActiveerKnop : MonoBehaviour
+{
+    public GameObject AllParticles;
+    public GameObject Audio;
+    public GameObject CutsceneCamera;
+    public GameObject PlayerCamera;
+    public GameObject Timeline;
+
+    void OnCollisionEnter()
+    {
+        AllParticles.SetActive(true);
+        Audio.SetActive(true);
+        CutsceneCamera.SetActive(true);
+        Timeline.SetActive(true);
+        PlayerCamera.SetActive(false);
+    }
+}
